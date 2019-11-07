922414c790213201f139499883d798f2d1972234
See Yang <seeyang24@gmail.com>
2019-11-07 16:12:31 -0600

    added tooltips to about me

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -2,12 +2,21 @@ import React, {Component} from 'react';
 import { Element } from 'react-scroll';
 import Profile from '../Images/see.jpg';
 
-import { Grid, Avatar, Typography, Divider } from '@material-ui/core';
+import { Grid, Avatar, Typography, Divider, Tooltip } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 import './AboutMe.css';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+
+import Reactjs from '../Icons/react.svg';
+import JavaScript from '../Icons/javascript.svg';
+import Node from '../Icons/nodejs-1-logo.png';
+import Reduxx from '../Icons/redux.svg';
+import Postgres from '../Icons/postgresql.png';
+import PHP from '../Icons/php.png';
+import CSS from '../Icons/CSS.3.svg';
+import HTML from '../Icons/html5.svg';
 
 const font = "'Dancing Script', sans-serif"; 
 
@@ -15,6 +24,14 @@ const theme = createMuiTheme({
     typography: {
       fontFamily: font
     },
+    overrides: {
+        MuiTooltip: {
+          tooltip: {
+            fontSize: "14px",
+            fontFamily: 'default'
+          }
+        }
+      }
   });
 
   const styles = theme => ({
@@ -29,17 +46,37 @@ const theme = createMuiTheme({
 		height: '300px',
 		width: '280px',
         margin: 'auto',
+        [theme.breakpoints.down('sm')]: {
+            height: '220px',
+            width: '200px'
+		}
     },
     title: {
 		fontWeight: 'bolder',
         color: 'black',
         fontSize: 55,
         paddingTop: '80px',
-        paddingBottom: '30px'
+        paddingBottom: '30px',
+    },
+    getToKnow: {
+        [theme.breakpoints.down('sm')]: {
+			fontSize: 32
+		}
     },
     bio: {
         fontFamily: 'default',
-        fontSize: '18px'
+        fontSize: '18px',
+        [theme.breakpoints.down('sm')]: {
+			fontSize: '15px'
+		}
+    },
+    languages: {
+        fontFamily: 'default',
+        fontSize: '18px',
+        fontWeight: 'bolder',
+        [theme.breakpoints.down('sm')]: {
+			fontSize: '15px'
+		}
     },
     grid: {
         width: '700px',
@@ -49,6 +86,25 @@ const theme = createMuiTheme({
     },
     dividerColor: {
         backgroundColor: 'gold',
+    },
+    icons: {
+        height: '50px',
+        width: '50px',
+        fontSize: '50px',
+        fontFamily: 'default',
+        align: 'center',
+        margin: theme.spacing(1),
+        justify: 'center',
+        alignItems: 'center',
+        [theme.breakpoints.down('sm')]: {
+            height: '35px',
+            width: '35px'
+		}
+    },
+    iconGrid: {
+        align:"center",
+        justify: 'center',
+        margin: 'auto',
     }
 });
 
@@ -74,17 +130,47 @@ class AboutMe extends Component {
                         </Grid>
                         <br />
                         <Grid item xs={6} className={classes.grid}>
-                            <Typography variant='h3' align='center' fontFamily='default'>
+                            <Typography variant='h3' align='center' fontFamily='default' className={classes.getToKnow}>
                                 Get to Know Me
                             </Typography>
                             <Divider className={classes.dividerColor}/>
+                            <br />
                             <Typography paragraph align='center' className={classes.bio} >
-                                Welcome! My name is See. I am a new and ever-curious Software Developer based in Minneapolis, MN.
+                                <strong>Welcome!</strong> My name is See. I am a new and ever-curious Software Developer based in Minneapolis, MN.
                                 <br />
-                                I enjoy learning and making websites, both on the front-end and back-end.
+                                I enjoy learning new materials and designing websites, both on the front-end and back-end.
                                 <br />
                                 When I am not coding, I am busy hiking, planning my future travels, or spending time with my family.
                             </Typography>
+                        </Grid>
+                        <Grid item xs={8}  className={classes.grid} align="center">
+                            <Typography paragraph align='center' className={classes.languages} >
+                                Languages and Tools:
+                            </Typography>
+                            <Tooltip className={classes.icons} title="ReactJS" >
+                                <img src={Reactjs} alt="reactIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="JavaScript" >
+                                <img src={JavaScript} alt="JavaScriptIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="Node.JS" >
+                                <img src={Node} alt="NodeJSIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="Redux" >
+                                <img src={Reduxx} alt="ReduxIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="PostgreSQL" >
+                                <img src={Postgres} alt="PostgreSQLIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="PHP" >
+                                <img src={PHP} alt="PHPIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="CSS" >
+                                <img src={CSS} alt="cssIcon"/>
+                            </Tooltip>
+                            <Tooltip className={classes.icons} title="HTML" >
+                                <img src={HTML} alt="htmlIcon"/>
+                            </Tooltip>
                         </Grid>
                     </div>
                 </ThemeProvider>
